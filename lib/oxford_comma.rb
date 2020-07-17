5af06544b97de2ab77b1ab38174e4e4379e61012
Cat Gomez <catherinedgomez@gmail.com>
2020-07-17 11:41:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,17 @@
 def oxford_comma(array)
+<<<<<<< HEAD
   if array.length == 1
     array.join
   elsif array.length == 2
     array.join(" and ")
   else array.length >= 3
   array.slice(0..-2).join(", ") + ", and #{array.last}"
+=======
+  if array
+    return [0]
+  elsif array.length = 1
+    array.join(" and ")
+  else array
+>>>>>>> 228cf37b1ca5f724a700d79e3002237f1bd72332
   end
 end
